7d3e4ee6db7cd2a6e426f98a8ef244adbb1fc613
Alexander Becker <alexbecker@Alexanders-MacBook-Pro-2.local>
2022-07-02 18:48:32 -0700

    Set up basic express server

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+// Require in dependencies
+require("dotenv").config();
+const express = require("express");
+const logger = require("morgan");
+
+// Define port
+const PORT = process.env.PORT;
+
+// Create express app
+const app = express();
+
+// Use logger middleware
+app.use(logger("dev"));
+
+// Basic get route
+app.get("/", function (req, res) {
+  res.send("hello, earth!");
+});
+
+// Start app on port
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
