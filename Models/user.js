90466de4d0c45e81e1c5f688ffd649c8b12f9e48
Alexander Becker <alexbecker@Alexanders-MacBook-Pro-2.local>
2022-07-03 16:11:45 -0700

    Added user model

---
+++ Models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("../db/connection.js");
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+});
+
+module.exports = mongoose.model("User", userSchema);
