43b9376a05f6c84768e2269c3600f04e0f91cc79
Alexander Becker <alexbecker@Alexanders-MacBook-Pro-2.local>
2022-07-06 02:33:56 -0700

    Updated user auth model and routes

--- Controllers/users.js
+++ Controllers/users.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const User = require("../Models/user.js");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
 
 //Register user
 router.post("/register", async (req, res) => {
   //get new user data
-  const salt = await bcrypt.genSalt(10);
-  const hashPw = await bcrypt.hash(req.body.password, salt);
+  //   const salt = await bcrypt.genSalt(10);
+  //   const hashPw = await bcrypt.hash(req.body.password, salt);
 
   const userObj = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
-    password: req.body.hashPw,
+    password: req.body.password,
   };
   //Save user to db
   User.create(userObj).then((user) => {
@@ -23,62 +23,15 @@ router.post("/register", async (req, res) => {
   });
 });
 
-//Login
-router.post("/login", async (req, res) => {
-  //Find all users
-  const user = await User.findOne({ email: req.body.email });
-  const validPass = await bcrypt.compare(req.body.passord, user.password);
-  console.log(user, req.body.email, "valid:", validPass);
-  if (!user || !validPass)
-    res.status(400).send("Email or password is incorrect");
-  if (user && validPass) res.status(200).send("logged in");
-});
-
-// // GET /users
-// router.get("/", function (req, res) {
-//   // Find all the users
-//   User.find({})
-//     // Return users as json
-//     .then((users) => res.status(200).json({ users: users }));
-// });
-
-// // POST /user
-// router.post("/", function (req, res) {
-//   // get new user data
-//   const data = req.body;
-//   // save user to db
-//   User.create(data).then((user) => {
-//     res.status(201).json({ user: user });
-//     console.log(user);
-//   });
-// });
-
-// // GET /:id
-// router.get("/:id", function (req, res) {
-//   const id = req.params.id;
-//   //Find user by id
-//   User.findById(id)
-//     //Return user as json
-//     .then((user) => res.status(200).json({ user: user }));
-//   console.log(id);
-// });
-
-// // DELETE /:id
-// router.delete("/:id", (req, res) => {
-//   //Find user by id and delete
-//   User.findByIdAndDelete(req.params.id).then((user) => {
-//     res.json({ data: user });
-//   });
-// });
-
-// // PATCH /:id
-// router.patch("/:id", (req, res) => {
-//   //Find user by id and update
-//   User.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
-//     (user) => {
-//       res.json({ data: user });
-//     }
-//   );
+// //Login
+// router.post("/login", async (req, res) => {
+//   //Find all users
+//   const user = await User.findOne({ email: req.body.email });
+//   const validPass = await bcrypt.compare(req.body.passord, user.password);
+//   console.log(user, req.body.email, "valid:", validPass);
+//   if (!user || !validPass)
+//     res.status(400).send("Email or password is incorrect");
+//   if (user && validPass) res.status(200).send("logged in");
 // });
 
 module.exports = router;
--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost/myHealth-api");
+const dotenv = require("dotenv");
+// mongoose.connect("mongodb://localhost/myHealth-api");
 // mongoose.connect("mongodb://127.0.0.1/myHealth-api")
+
+dotenv.config();
+
+//Connect to db
+mongoose.connect(process.env.DB_CONNECT, () => console.log("connected to db"));
 
 module.exports = mongoose;
